9ce77c5f6219702fddde6d769923b891828edfe2
Mitsuru GIMA <gima0326@yahoo.co.jp>
2022-02-21 10:12:58 +0900

    Delete step3/todo-api directory

--- step3/todo-api/resources/migrations/01-create-todos.up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE todos (id INT NOT NULL AUTO_INCREMENT, task text, PRIMARY KEY(id));
--- step3/todo-api/src/todo_api/middleware.clj
+++
@@ -1,7 +0,0 @@
-(ns todo-api.middleware
-  (:require [integrant.core :as ig]
-            [ring.middleware.cors :refer [wrap-cors]]))
-
-;;(defmethod ig/init-key :todo-api.middleware/wrap-cors [_ config]
-(defmethod ig/init-key ::wrap-cors [_ config]
-  #(apply wrap-cors % (apply concat config)))
